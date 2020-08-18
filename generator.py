00489a2aac71a715e9b8165b661f36e75f0bd25a
aman <aman@email.com>
2020-08-18 19:44:53 +0530

    added customization in varibales
    now you can select if var will appear in getter, setter or in constructor

--- generator.py
+++ generator.py
@@ -1,58 +1,96 @@
+# version 1.2
+
+# whats new ??
+# created command line argment
+# now you can customize the varibale u want to create constructor , getter or setter ( just like eclipse )
 
 import sys
 
-def getClass(cls,varss):
-    # create class 
-    print(f"class {cls}{{")
+def createConstructor(clss, cvars):
+    print("\n\t//constructor")
+    print(f"\tpublic {clss}(", end="")
+    param = ""
+    for var in cvars:
+        param += var + ", "
+    print(param[:-2],"){")
+
+    # constructor vars
+    for var in cvars:
+        _,name = var.split()
+        print(f"\t\tthis.{name} = {name};")
+    print("\t}")
+
+def createGetters(gvars):
+    # getters
+    print("\n\t//getters")
+    for var in gvars:
+        tpe,name = var.split()
+        print(f"\tpublic {tpe} get{name.title()}(){{\n\t\t return this.{name};\n\t}}")
+
+def createSetters(svars):
+    # setters
+    print("\n\t//setters")
+    for var in svars:
+        _,name = var.split()
+        print(f"\tpublic void set{name.title()}({var}){{\n\t\t this.{name} = {name};\n\t}}")
+
+
+
+def createClass(clss, varss, cvars, gvars, svars):
+    # begin class 
+    print(f"class {clss}{{")
     
     #print vars
     for var in varss:
         print("\tprivate "+var,end=';\n')
 
-    #creat constructors
-    print("\n\t//constructor")
-    print(f"\tpublic {cls}(", end="")
-    param = ""
-    for var in varss:
-        param += var + ", "
-    print(param[:-2],"){")
+    createConstructor(clss, cvars)
+    createGetters(gvars)
+    createSetters(cvars)
 
-    # constructor vars
-    for var in varss:
-        tpe,name = var.split()
-        print(f"\t\tthis.{name} = {name};")
-    print("\t}")
-
-    # getters
-    print("\n\t//getters")
-    for var in varss:
-        tpe,name = var.split()
-        print(f"\tpublic {tpe} get{name.title()}(){{\n\t\t return this.{name};\n\t}}")
-
-    # setters
-    print("\n\t//setters")
-    for var in varss:
-        tpe,name = var.split()
-        print(f"\tpublic void set{name.title()}({var}){{\n\t\t this.{name} = {name};\n\t}}")
-
+    # end class
     print("}")
 
 
-def createSolution(cls,varss):
-    getClass(cls,varss)
+def createSolution(clss,varss,cvars,gvars,svars) :
+    createClass(clss,varss,cvars,gvars,svars)
     print("\npublic class Solution{")
     print("\tpublic static void main(String[] argv){\n\t\t\n\t}\n}")
 
 
-if __name__ == "__main__":
-    cls = sys.argv[1]
-    n = len(sys.argv)
+def go():
+    argv = sys.argv[1:]
+    usage = '.....error.....'
+    clss = ''
     varss = []
-    for i in range(2,n):
-        varss.append(sys.argv[i])
+    gvars = []
+    svars = []
+    cvars = []
+    i = 0
+    while i < len(argv) :
+        if argv[i] == '-c' :
+            clss = argv[i+1]
+#            print(i,'cls')
+            i = i + 2
 
-#    print(cls,varss)
-    getClass(cls,varss)
-        
+        elif argv[i].startswith('-v'):
+            varss.append(argv[i+1])
+#            print(i,argv[i])
+            if 'g' in argv[i] : 
+                gvars.append(argv[i+1])
+            if 's' in argv[i] : 
+                svars.append(argv[i+1])
+            if 'c' in argv[i]:
+                cvars.append(argv[i+1])
+            i = i + 2
 
+        else :
+            print(usage)
+            break
+#    print(argv, clss, varss, cvars, gvars, svars, sep='\n')
+    createClass(clss,varss,cvars,gvars,svars)
+
+
+if __name__ == "__main__":
+    go()
 
